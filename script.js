7071aecf0143ee5970d165d8746655e46a426b16
Stormer <tranhocpro2@gmail.com>
2023-12-18 10:25:29 +0700

    update 10:25 18/12/2023

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
     $('.customer').slick({
       dots: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="src/leftarrow.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="src/rightarrow.svg"></button>',
       });
 
   });
 
+  document.getElementById("topnav__right__btn").addEventListener("click", function() {
+    var topnavcolumn = document.getElementById("topnav-column");
+
+    // Toggle the display style
+    if (topnavcolumn.style.display === "none" || topnavcolumn.style.display === "") {
+        topnavcolumn.style.display = "flex";        
+    } else {
+        topnavcolumn.style.display = "none";
+    }
+    });
